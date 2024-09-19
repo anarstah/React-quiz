eb4f3cdf4364d7b9ab9ad51b4e6fbfb3d4f2064d
Your Name <you@example.com>
2024-09-19 15:50:06 +0300

    init

---
+++ src/components/Timer.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+
+function Timer({ dispatch, secondsRemainig }) {
+  const mins = Math.floor(secondsRemainig / 60);
+  const seconds = secondsRemainig % 60;
+
+  useEffect(
+    function () {
+      const id = setInterval(function () {
+        dispatch({ type: "tick" });
+      }, 1000);
+
+      return () => clearInterval(id);
+    },
+    [dispatch]
+  );
+
+  return (
+    <div className="timer">
+      {mins < 10 && "0"}
+      {mins}:{seconds < 10 && "0"}
+      {seconds}
+    </div>
+  );
+}
+
+export default Timer;
